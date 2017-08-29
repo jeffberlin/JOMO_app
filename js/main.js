983c4f5f9a9ac8d93516c6e5f2a0ce51d6377214
Jeff Berlin <jeff@jbrally.com>
2017-08-29 11:54:24 -0400

    Fixed phone names

--- js/main.js
+++ js/main.js
@@ -205,21 +205,21 @@ function ParseChar(sStr, sChar) {
 }
 
 // "TIME" flipper section
-var wordList = [
-    'Sports', 'Art', 'Friends', 'Music', 'Brunch', 'Games', 'Family', 'Racing', 'Fitness', 'Crafts', 'Surfing', 'Time'
-]
-var counter = 0;
+// var wordList = [
+//     'Sports', 'Art', 'Friends', 'Music', 'Brunch', 'Games', 'Family', 'Racing', 'Fitness', 'Crafts', 'Surfing', 'Time'
+// ]
+// var counter = 0;
 
-var picker = document.getElementById("word-picker");
-setInterval(changeText, 3000);
+// var picker = document.getElementById("word-picker");
+// setInterval(changeText, 3000);
 
-function changeText() {
-    picker.innerHTML = wordList[counter];
-    counter++;
-    if (counter >= wordList.length) {
-        counter = 0;
-    }
-}
+// function changeText() {
+//     picker.innerHTML = wordList[counter];
+//     counter++;
+//     if (counter >= wordList.length) {
+//         counter = 0;
+//     }
+// }
 
 // OS detection
 var isMobile = {
