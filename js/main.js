11a306df14b6777d0c3e5bfcf7c19988371dc32f
Jeff Berlin <jeff@jbrally.com>
2017-08-29 11:50:30 -0400

    Change design...again

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ for (var i = 0; i < links.length; i++) {
 	links[i].target = "_blank";
 }
 
-// Handles phone number input
+// Handles phone number input format
 var zChar = new Array(' ', '(', ') ', '-', '.');
 var maxphonelength = 14;
 var phonevalue1;
@@ -220,3 +220,45 @@ function changeText() {
         counter = 0;
     }
 }
+
+// OS detection
+var isMobile = {
+    getUserAgent: () => {
+        return navigator.userAgent;
+    },
+     Android: function() {
+        return /Android/i.test(isMobile.getUserAgent()) && !isMobile.Windows();
+    },
+    BlackBerry: function() {
+        return /BlackBerry|BB10|PlayBook/i.test(isMobile.getUserAgent());;
+    },
+    iPhone: function() {
+        //console.log("iphone");
+        return /iPhone/i.test(isMobile.getUserAgent()) && !isMobile.iPad() && !isMobile.Windows();
+
+    },
+    iPod: function() {
+        return /iPod/i.test(isMobile.getUserAgent());
+    },
+    iPad: function() {
+        return /iPad/i.test(isMobile.getUserAgent());
+    },
+    iOS: function() {
+        console.log("iphone");
+        return (isMobile.iPad() || isMobile.iPod() || isMobile.iPhone());
+    },
+    Opera: function() {
+        return /Opera Mini/i.test(isMobile.getUserAgent());
+    },
+    Windows: function() {
+        return /Windows Phone|IEMobile|WPDesktop/i.test(isMobile.getUserAgent());
+    },
+    KindleFire: function() {
+        return /Kindle Fire|Silk|KFAPWA|KFSOWI|KFJWA|KFJWI|KFAPWI|KFAPWI|KFOT|KFTT|KFTHWI|KFTHWA|KFASWI|KFTBWI|KFMEWI|KFFOWI|KFSAWA|KFSAWI|KFARWI/i.test(isMobile.getUserAgent());
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+//module.exports = isMobile;
